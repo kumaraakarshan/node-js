d7d52402c3d0651a461c39e055e22a65030b458a
kumaraakarshan <aakarshan0005@gmail.com>
2023-08-06 14:21:31 +0530

    add product configured

--- play.js
+++ play.js
@@ -1,18 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
 
-const express=require('express');
+app.use(bodyParser.urlencoded({ extended: false }));
 
-
-const app= express();
-
-app.use((req, res, next)=>{
-console.log('middle ware!!!');
-next();
+// Middleware 1
+app.use('/', (req, res, next) => {
+  console.log('always run!!!');
+  next(); // Call next() to pass control to the next middleware
 });
-app.use((req, res, next)=>{
-    console.log('another middle ware!!!');
 
-    res.send('<h1> hello from express<h1>');
-    });
+// Middleware 2
+app.get('/add-product', (req, res, next) => {
+  console.log('in another middleware');
+  res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title">
+      <button type="submit">Add Product</button>
+    </form>
+  `);
+});
 
-app.listen(4000);
+// Middleware 3
+app.post('/product', (req, res, next) => {
+  console.log('Form data:', req.body); // This will log the parsed form data to the console
+  res.redirect('/');
+});
+
+// Middleware 4
+app.use('/', (req, res, next) => {
+  console.log('another middle ware!!!');
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
