3b74918559803fb08034d3c91ccf88ce304501d5
kumaraakarshan <aakarshan0005@gmail.com>
2023-08-06 17:11:16 +0530

    admin shop added

--- play.js
+++ play.js
@@ -3,36 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
- // console.log('always run!!!');
-  next(); 
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-
-app.get('/add-product', (req, res, next) => {
-  //console.log('in another middleware');
-  res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title">
-      <button type="submit">Add Product</button>
-    </form>
-  `);
-});
-
-app.post('/product', (req, res, next) => {
-  console.log('Form data:', req.body.title);
-  res.redirect('/');
-});
-
-
-app.use('/', (req, res, next) => {
-  //console.log('another middle ware!!!');
-  res.send('<h1>Hello from Express!</h1>');
-});
-
-app.listen(4000, () => {
- // console.log('Server is running on port 4000');
-});
+app.listen(4000);
